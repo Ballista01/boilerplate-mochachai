802ba39b5fe3a70341484c812f6beb6521d13504
Wenxue Zhao <ballista01@outlook.com>
2021-12-21 02:40:38 -0500

    all test passed on local machine, replit still crashes due to unknown server crash during last two tests - fix comment

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -101,15 +101,15 @@ suite('Functional Tests with Zombie.js', function () {
       });
     });
     // #6
-    // test('Submit the surname "Vespucci" in the HTML form', function (done) {
-    //   browser.fill('surname', 'Vespucci');
-    //   browser.pressButton('submit', () => {
-    //     browser.assert.success();
-    //     browser.assert.text('span#name', 'Amerigo');
-    //     browser.assert.text('span#surname', 'Vespucci');
-    //     browser.assert.elements('span#dates', 1);
-    //     done();
-    //   });
-    // });
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      browser.fill('surname', 'Vespucci');
+      browser.pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
+        done();
+      });
+    });
   });
 });
